2c2ee90bd5af4ea98123e37fdc9f63c5d8f8e661
saadamjad <46295251+saadamjad@users.noreply.github.com>
2020-11-24 11:49:05 +0500

    update

--- src/redux/actions/types.js
+++
@@ -1,6 +0,0 @@
-// export const STARTLOADER = 'STARTLOADER';
-// export const STOPLOADER = 'STOPLOADER';
-// export const USERDATA = 'USERDATA';
-// export const UID = 'UID';
-// export const MYDETAILS = 'MYDETAILS';
-// export const ISLOGGEDIN = 'ISLOGGEDIN';

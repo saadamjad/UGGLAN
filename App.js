883c50444ba82ce2fe8d7d4f141e0e5ae2a5ea2c
Syed Asim <syedasimurrehman@gmail.com>
2021-01-22 17:59:04 +0500

    Push Notifications Done

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Alert
 } from 'react-native';
 import 'react-native-gesture-handler';
 import {
@@ -27,8 +28,83 @@ import Stack from './src/navigations/stacknavigation';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './src/store/store';
+import OneSignal from 'react-native-onesignal';
 
 const App = (props) => {
+  const [state,setState]=useState({
+    requiresPrivacyConsent :'',
+    isSubscribed:''
+  })
+  useEffect(async () => {
+    /* O N E S I G N A L   S E T U P */
+    OneSignal.setAppId('72213651-048b-4a7d-a149-25bbc0a014eb');
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setRequiresUserPrivacyConsent(state.requiresPrivacyConsent);
+    OneSignal.postNotification((response) => {
+      OSLog('Prompt response:', response);
+    });
+
+    /* O N E S I G N A L  H A N D L E R S */
+    OneSignal.setNotificationWillShowInForegroundHandler(
+      (notifReceivedEvent) => {
+        OSLog(
+          'OneSignal: notification will show in foreground:',
+          notifReceivedEvent,
+        );
+        let notif = notifReceivedEvent.getNotification();
+
+        const button1 = {
+          text: 'Cancel',
+          onPress: () => {
+            notifReceivedEvent.complete();
+          },
+          style: 'cancel',
+        };
+
+        const button2 = {
+          text: 'Complete',
+          onPress: () => {
+            notifReceivedEvent.complete(notif);
+          },
+        };
+
+        Alert.alert('Complete notification?', 'Test', [button1, button2], {
+          cancelable: true,
+        });
+      },
+    );
+    OneSignal.setNotificationOpenedHandler((notification) => {
+      OSLog('OneSignal: notification opened:', notification);
+    });
+    OneSignal.setInAppMessageClickHandler((event) => {
+      OSLog('OneSignal IAM clicked:', event);
+    });
+    OneSignal.addEmailSubscriptionObserver((event) => {
+      OSLog('OneSignal: email subscription changed: ', event);
+    });
+    OneSignal.addSubscriptionObserver((event) => {
+      OSLog('OneSignal: subscription changed:', event);
+      setState({isSubscribed: event.to.isSubscribed});
+    });
+    OneSignal.addPermissionObserver((event) => {
+    OSLog('OneSignal: permission changed:', event);
+    });
+
+    const deviceState = await OneSignal.getDeviceState();
+
+    setState({
+      isSubscribed: deviceState.isSubscribed,
+    });
+  }, []);
+
+  // useEffect(()=>{
+  //   OneSignal.setAppId("72213651-048b-4a7d-a149-25bbc0a014eb");
+  //   OneSignal.setLogLevel(6, 0);
+  //   OneSignal.setRequiresUserPrivacyConsent(this.state.requiresPrivacyConsent);
+  //   OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  //       this.OSLog("Prompt response:", response);
+  // },[])
+
   return (
     <>
       <Provider store={store}>
